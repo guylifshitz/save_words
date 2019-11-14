741c86d0a5d0dd4ab0f5fdfb47bdc4fac0dda8fe
Guy <guylifshitz@gmail.com>
2019-11-14 23:44:36 +0100

    Get a basic form to submit.

--- popup/popup.js
+++ popup/popup.js
@@ -1,22 +1,46 @@
-console.log(selected_text);
+var word = window.getSelection();
+var sentence = window.getSelection()["anchorNode"]["data"];
 
-var popup = document.createElement('div');
+var popup = document.createElement("div");
 popup.classList = ["french_word_popup"];
-
-var popup_title = document.createElement("h2");
-popup_title.innerHTML = selected_text;
+var popup_title = document.createElement("div");
 popup.appendChild(popup_title);
-
 document.body.appendChild(popup);
 
+form_html = `<form id="sendWord_form">
+  Word:<br>
+  <input type="text" name="word" value="${word}">
+  <br>
+  Example:<br>
+  <input type="text" name="example" value="${sentence}">
+  <br><br>
+  <input type="submit" value="Submit">
+  <br><br>
+  <div id=sendWord_form_cancel>Cancel</div>
+</form> 
+`;
 
-// function submitGoogleForm ($id, $post){
-//     $formUrl = 'https://docs.google.com/a/vcu.edu/forms/d/e/1FAIpQLScK2wgma6Oicv_ZY9i-6tg_w9RfEKKkgiAFJDw15jJnmr5ofQ/formResponse?usp=pp_url&entry.1431785794=';
-//     $projectName =$post->post_name;
-//     $submit = '&submit=Submit';
-//     $fullUrl = $formUrl.$projectName.$submit;
- 
-//     file_get_contents($fullUrl);
-// }
+popup_title.innerHTML = form_html;
 
-// add_action( 'publish_project', 'submitGoogleForm', 10, 2);
+document
+  .querySelector("#sendWord_form_cancel")
+  .addEventListener("click", function(e) {
+    document.body.removeChild(popup);
+  });
+
+document
+  .querySelector("#sendWord_form")
+  .addEventListener("submit", function(e) {
+    e.preventDefault();
+    var url = `https://docs.google.com/forms/d/e/1FAIpQLSdrJIcpHubmuG_RVvaePXaY--V2S52McKI6me5BrAOaqMU7TA/formResponse?entry.318015364=${word}&entry.1975522348=Gender&entry.1571542855=Masc&entry.1852938782=${sentence}&entry.376664097=asd&entry.695391719=asdasd&submit=Submit`;
+
+    function reqListener() {
+      console.log(this.responseText);
+      document.body.removeChild(popup);
+    }
+
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", url);
+    oReq.send();
+  });
